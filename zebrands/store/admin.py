3dde9f38eee3bb8ff10edfe0533c1a144ca938e1
mbarcelo <mbarcelo@talpor.com>
2023-03-31 19:28:18 -0300

    + Add Product model

--- zebrands/store/admin.py
+++ zebrands/store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["sku", "name", "brand", "price"]
--- zebrands/store/models.py
+++ zebrands/store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from model_utils.models import TimeStampedModel
+
+
+class Product(TimeStampedModel):
+    sku = models.CharField(max_length=32, null=False)
+    name = models.CharField(max_length=256, null=False)
+    price = models.DecimalField(max_digits=16, decimal_places=2, null=False)
+    brand = models.CharField(max_length=128, null=False)
+
+    def __str__(self):
+        return f"{self.sku} - {self.brand} - {self.name}"
