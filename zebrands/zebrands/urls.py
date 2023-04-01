b40fac15dbc2e73deee58afeb8224803e70137d5
mbarcelo <mbarcelo@talpor.com>
2023-03-31 23:14:42 -0300

    + Add DRF router + Swagger

--- zebrands/zebrands/urls.py
+++ zebrands/zebrands/urls.py
@@ -14,8 +14,37 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions, routers
+from store.views import ProductViewSet
+
+router = routers.DefaultRouter()
+router.register(r"products", ProductViewSet, basename="products")
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Store API",
+        default_version="v1",
+        description="Test description",
+        contact=openapi.Contact(email="mauricio@challenge.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+
+urlpatterns = router.urls + [
+    re_path(
+        r"^doc/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
+    path("admin/", admin.site.urls),
 ]
