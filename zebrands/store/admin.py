f86ab0d9ed0025f625f7fc4b4d715a6bfd3b5f87
mbarcelo <mbarcelo@talpor.com>
2023-04-02 13:52:22 -0300

    + Register Stats in admin

--- zebrands/store/admin.py
+++ zebrands/store/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from store.models import Product
+from store.models import Product, ProductStats
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["sku", "name", "brand", "price"]
+
+
+@admin.register(ProductStats)
+class ProductStatsAdmin(admin.ModelAdmin):
+    list_display = ["product", "view_count"]
