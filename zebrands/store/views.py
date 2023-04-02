cfd5c21a53097f4680948c1d42a8940354a77ea1
mbarcelo <mbarcelo@talpor.com>
2023-04-01 21:15:06 -0300

    + Permissions rules in products views

--- zebrands/store/views.py
+++ zebrands/store/views.py
@@ -1,23 +1,29 @@
-from rest_framework import viewsets
-from rest_framework.mixins import (
-    CreateModelMixin,
-    DestroyModelMixin,
-    RetrieveModelMixin,
-    UpdateModelMixin,
-)
-from rest_framework.permissions import AllowAny
-from store.models import Product
-from store.serializers import ProductSerializer
+from rest_framework.permissions import AllowAny, BasePermission, IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from store.models import PRODUCT_ADMIN_GROUP, Product
+from store.serializers import ProductListSerializer, ProductSerializer
 
 
-class ProductViewSet(
-    CreateModelMixin,
-    UpdateModelMixin,
-    DestroyModelMixin,
-    RetrieveModelMixin,
-    viewsets.GenericViewSet,
-):
-    serializer_class = ProductSerializer
+class ProductAdminOnly(BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        return user.groups.filter(name=PRODUCT_ADMIN_GROUP).exists()
+
+
+class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
-    permission_classes = (AllowAny,)
     lookup_field = "sku"
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ProductListSerializer
+        return ProductSerializer
+
+    def get_permissions(self):
+        if self.action in ("retrieve", "list"):
+            # Allow anonymous users to access the retrieve method
+            permission_classes = [AllowAny]
+        else:
+            # Allow access only to authenticated users in the ProductAdmin group
+            permission_classes = [IsAuthenticated, ProductAdminOnly]
+        return [permission() for permission in permission_classes]
