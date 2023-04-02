f85d8f99b55fc847d22478c038f8bcee945d7987
mbarcelo <mbarcelo@talpor.com>
2023-04-02 13:53:27 -0300

    + Add more fields to user serializer

--- zebrands/users/serializers.py
+++ zebrands/users/serializers.py
@@ -4,9 +4,16 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True, max_length=128)
+    username = serializers.CharField(required=True, max_length=128)
+    first_name = serializers.CharField(required=False, max_length=128)
+    last_name = serializers.CharField(required=False, max_length=128)
+
     class Meta:
         model = User
-        fields = ["username", "email"]
+        read_only_fields = ["username"]
+        fields = ["email", "password", "username", "first_name", "last_name"]
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
