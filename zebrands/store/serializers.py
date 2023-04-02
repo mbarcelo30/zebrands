a65caaf155c68957931f2e65f057b871997a01da
mbarcelo <mbarcelo@talpor.com>
2023-04-01 21:13:23 -0300

    + Add serializer for Product list

--- zebrands/store/serializers.py
+++ zebrands/store/serializers.py
@@ -2,7 +2,17 @@ from rest_framework import serializers
 from store.models import Product
 
 
+class ProductListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ("sku", "name")
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    price = serializers.DecimalField(
+        required=True, allow_null=False, max_digits=16, decimal_places=2
+    )
+
     class Meta:
         model = Product
         fields = ("sku", "name", "price", "brand")
