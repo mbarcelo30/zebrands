9b2c9824c9088e3559e3acbd74c3c6f5c7a16bca
mbarcelo <mbarcelo@talpor.com>
2023-04-04 13:17:05 -0300

    + Change swagger url endpoints

--- zebrands/zebrands/urls.py
+++ zebrands/zebrands/urls.py
@@ -36,12 +36,12 @@ urlpatterns = router.urls + [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     # Optional UI:
     path(
-        "api/schema/swagger-ui/",
+        "api/doc/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
     path(
-        "api/schema/redoc/",
+        "api/redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
