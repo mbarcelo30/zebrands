89fa5ad6f6a77998976fbfacf6b2e789d67d7514
mbarcelo <mbarcelo@talpor.com>
2023-04-05 17:39:05 -0300

    + Add settings for environments

--- zebrands/zebrands/asgi.py
+++ zebrands/zebrands/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zebrands.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zebrands.settings.development")
 
 application = get_asgi_application()
