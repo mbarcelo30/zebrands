b65dc33a7ac1914d77e424aa1e9ebe753b7112ce
mbarcelo <mbarcelo@talpor.com>
2023-04-06 13:32:45 -0300

    + Comments for views

--- zebrands/store/views.py
+++ zebrands/store/views.py
@@ -7,7 +7,21 @@ from store.tasks import product_update_counter
 
 
 class ProductAdminOnly(BasePermission):
+    """
+    Permission class that allows access only to users in the 'ProductAdmin' group.
+    """
+
     def has_permission(self, request, view):
+        """
+        Returns True if the user making the request is a member of the 'ProductAdmin' group.
+
+        Args:
+        - request: The HTTP request object.
+        - view: The view object associated with the request.
+
+        Returns:
+        True if the user is a member of the 'ProductAdmin' group, False otherwise.
+        """
         user = request.user
         return user.groups.filter(name=PRODUCT_ADMIN_GROUP).exists()
 
@@ -17,11 +31,26 @@ class ProductViewSet(ModelViewSet):
     lookup_field = "sku"
 
     def get_serializer_class(self):
+        """
+        Returns the serializer class to use, depending on the requested action.
+
+        If the action is 'list', returns the serializer class for the list of products.
+        Otherwise, returns the serializer class for an individual product.
+        """
         if self.action == "list":
             return ProductListSerializer
         return ProductSerializer
 
     def get_permissions(self):
+        """
+        Obtains a list of permissions that are required to perform the current action.
+
+        For non-authenticated users, the 'list' and 'retrieve' actions are allowed.
+        For authenticated users, only those in the 'ProductAdmin' group are allowed to access other actions.
+
+        @return:
+        A list of permission objects.
+        """
         if self.action in ("retrieve", "list"):
             # Allow anonymous users to access the retrieve method
             permission_classes = [AllowAny]
@@ -31,6 +60,14 @@ class ProductViewSet(ModelViewSet):
         return [permission() for permission in permission_classes]
 
     def retrieve(self, request, *args, **kwargs):
+        """
+        Retrieves a single product instance.
+
+        If the user is not authenticated, updates the product's view counter by scheduling a task using Celery.
+
+        Returns:
+        A response object containing the serialized product instance data.
+        """
         instance = self.get_object()
         is_authenticated = request.user.is_authenticated
         if not is_authenticated:
